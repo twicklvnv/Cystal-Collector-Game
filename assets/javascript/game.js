4424f4f6d0dbc92bee676dc6e2b8f08fdfc2ecef
unknown <twicklvnv@yahoo.com>
2017-12-03 09:37:37 -0700

    final commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,15 @@
 //creating variables to be used in game
 
 var randomNumber;
-var score;
+var score = 0;
 var crystalValue;
-var wins;
-var losses;
+var wins = 0;
+var losses = 0;
 var crystalNumber = [];
+var total = 0;
+
+$(".numberWins").text(wins);
+$(".numberLosses").text(losses);
 
 //defining randomNumber as a random number between 19-120
 randomNumber = Math.floor(Math.random() * 102) + 19;
@@ -21,3 +25,93 @@ function randomCrystalNumbers() {
 
 randomCrystalNumbers();
 console.log(crystalNumber);
+
+//starts new game and resets numbers
+function restart() {
+	randomNumber = Math.floor(Math.random() * 102) + 19;
+	console.log(randomNumber);
+	$(".ranNum").text(randomNumber);
+	crystalNumber = [];
+	randomCrystalNumbers();
+	console.log(crystalNumber);
+	score = 0;
+	total = 0;
+	$(".curScore").text(score);
+	//$(".declareResult").text("");
+}
+
+restart();
+
+//function to add wins to total and set new game
+function youWin() {
+	wins++;
+	$(".declareResult").text("You Win!");
+	$(".numberWins").text(wins);
+	restart();
+}
+
+//function to add losses to total and set new game
+function youLose() {
+	losses++;
+	$(".declareResult").text("You Lose.");
+	$(".numberLosses").text(losses);
+	restart();
+}
+
+//click function and assigning value to crystals
+	$("#crystalOne").on("click", function() {
+		$(".declareResult").text(" ");
+		crystalValue = crystalNumber[0];
+		console.log(crystalValue);
+		total = total + crystalValue;
+		$(".curScore").text(total);
+		if (total == randomNumber) {
+			youWin();
+		}
+		else if (total > randomNumber) {
+			youLose();
+		}
+		//$(".declareResult").text(" ");
+	})
+
+	$("#crystalTwo").on("click", function() {
+		$(".declareResult").text(" ");
+		crystalValue = crystalNumber[1];
+		total = total + crystalValue;
+		$(".curScore").text(total);
+		if (total == randomNumber) {
+			youWin();
+		}
+		else if (total > randomNumber) {
+			youLose();
+		}
+		//$(".declareResult").text(" ");
+	})
+
+	$("#crystalThree").on("click", function() {
+		$(".declareResult").text(" ");
+		crystalValue = crystalNumber[2];
+		total = total + crystalValue;
+		$(".curScore").text(total);
+		if (total == randomNumber) {
+			youWin();
+		}
+		else if (total > randomNumber) {
+			youLose();
+		}
+		//$(".declareResult").text(" ");
+	})
+
+	$("#crystalFour").on("click", function() {
+		$(".declareResult").text(" ");
+		crystalValue = crystalNumber[3];
+		total = total + crystalValue;
+		$(".curScore").text(total);
+		if (total == randomNumber) {
+			youWin();
+		}
+		else if (total > randomNumber) {
+			youLose();
+		}
+		//$(".declareResult").text(" ");
+	})
